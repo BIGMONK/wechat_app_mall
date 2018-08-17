a71f3a21d4ee96bb936ba361f857acf9f1c999ba
djf-ut-PC <488698112@qq.com>
2018-08-17 18:03:27 +0800

    second commit

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ App({
   globalData:{
     userInfo:null,
     subDomain: "6b3e12a235ce9f90eba3dfb5288b9bf2", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
+    // subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
     version: "3.1.0",
     shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
   }
